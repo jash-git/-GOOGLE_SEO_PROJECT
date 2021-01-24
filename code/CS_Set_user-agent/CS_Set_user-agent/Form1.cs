006ed8c7942af7aeec3f867a5ffea1c3ee1d713f
jash.liao <jash.liao@gmail.com>
2021-01-24 21:04:24 +0800

    Add 202101242104

--- code/CS_Set_user-agent/CS_Set_user-agent/Form1.cs
+++ code/CS_Set_user-agent/CS_Set_user-agent/Form1.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,22 +17,44 @@ namespace CS_Set_user_agent
 {
     public partial class Form1 : Form
     {
+        public bool[] m_blnPing = new bool[2] {false,false};
+        public static bool TcpSocketTest()
+        {
+            try
+            {
+                System.Net.Sockets.TcpClient client =
+                    new System.Net.Sockets.TcpClient("www.google.com", 80);
+                client.Close();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                return false;
+            }
+        }
         public ArrayList m_ALData = new ArrayList();
-        public int m_intCount = 0;
+        public int m_intCount = 1;
         public int m_ALindex = 0;
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            timer1.Enabled = false;
             label1.Text = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
-            if(m_intCount==0)
+            m_blnPing[1] = TcpSocketTest();
+            if ( (m_intCount==0) || (m_blnPing[1]^m_blnPing[0]))
             {
                 foreach (var process in Process.GetProcessesByName("brave"))
                 {
-                    process.Kill();
+                    try
+                    {
+                        process.Kill();
+                    }
+                    catch { }
                 }
 
                 Random R = new Random();
@@ -39,22 +62,24 @@ namespace CS_Set_user_agent
                 this.Text = m_ALindex.ToString();
                 textBox1.Text = m_ALData[m_ALindex].ToString();
                 ProcessStartInfo start = new ProcessStartInfo();
-                start.FileName = "C:\\Users\\jashliao\\Downloads\\brave-portable-win32-1.14.81-64\\brave-portable.exe";  // Specify exe name.
+                start.FileName = "C:\\Users\\user\\Downloads\\brave-portable-win64-1.14.81-64\\brave-portable.exe";  // Specify exe name.
                 start.Arguments = String.Format(" http://jashliao.eu/google_seo/index02.php http://jashliao.eu/google_seo/index02.php http://jashliao.eu/google_seo/index02.php http://jashliao.eu/google_seo/index02.php --user-agent=\"{0}\"", textBox1.Text);
                 start.UseShellExecute = false;
                 start.RedirectStandardOutput = false;
                 Process p01 = Process.Start(start);
                 this.WindowState = FormWindowState.Maximized;
-                this.TopMost = true;
+                //this.TopMost = true;
                 
             }
 
+            m_blnPing[0] = m_blnPing[1];
+
             m_intCount++;
-            if (m_intCount>60*30)
+            if (m_intCount>60*15)
             {
                 m_intCount = 0;
             }
-            
+            timer1.Enabled = true;
         }
         
         private void Form1_Load(object sender, EventArgs e)
